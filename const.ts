8bfae19e1b5ee1e3b396e8429f5ef1ad078d7273
JorensM <jorensmerenjanu@gmail.com>
2023-08-28 15:54:51 +0300

    update api keys

--- const.ts
+++ const.ts
@@ -2,7 +2,13 @@ const constant = {
     db: {
         id: '64e5bca5774c43c6e4b8',
         listings_id: '64e5bcac74d34020c488' 
+    },
+    api_keys: {
+        google: {
+            places: 'places'
+        }
     }
+        
 }
 
 export default constant
