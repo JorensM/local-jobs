a5bfebf9c6bc9afc4c6baa11bddee7698371014f
JorensM <jorensmerenjanu@gmail.com>
2024-01-16 10:22:53 +0200

    Add contact fetching functionality to my contacts page

--- app/src/app/contacts/my.tsx
+++ app/src/app/contacts/my.tsx
@@ -13,18 +13,49 @@ import list from '#styles/list';
 // Types
 import { User } from '#types/User';
 import { router } from 'expo-router';
+import useFocusEffect from '#hooks/useFocusEffect';
+import useContacts from '#hooks/useContacts';
+import { toastError } from '#misc/toast';
 
 
 export default function MyContactsPage() {
 
+    // Hooks
+    const contacts = useContacts();
+
+    // State
     const [contactsData, setContactsData] = useState<User[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
 
     const handleContactPress = (contact_id: string) => {
         router.replace('/contacts/' + contact_id);
     }
+
+    const fetchContacts = async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const _contacts = await contacts.fetchUsersContacts();
+            console.log(_contacts);
+            setContactsData(_contacts)
+            setLoading(false);
+        } catch (err: any) {
+            setError(err.message)
+            toastError('An error has occured', err.message);
+        }
+        
+    }
+
+    useFocusEffect(() => {
+        fetchContacts();
+    })
     
     return (
-        <SessionPage>
+        <SessionPage
+            loading={loading}
+            error={error}
+        >
             <FlatList
                 style={list.list}
                 ItemSeparatorComponent={() => <ListSeparator/>}
