67a7d9741ddf3b2d6cd00d7da0753bc1ea1fea88
JorensM <jorensmerenjanu@gmail.com>
2024-01-17 08:25:28 +0200

    scaffold profile page

--- app/src/app/profile.tsx
+++ app/src/app/profile.tsx
@@ -1,3 +1,28 @@
+// Components
+import SessionPage from '#components/layout/SessionPage';
+
+// Hooks
+import useFocusEffect from '#hooks/useFocusEffect';
+import useAuth from '#hooks/useAuth';
+import usePage from '#hooks/usePage';
+
 export default function ProfilePage() {
-    
+
+    // Hooks
+    const auth = useAuth();
+
+    // State
+    const { setError, setLoading, pageState } = usePage()
+
+    useFocusEffect(() => {
+        auth.fetchUser().then(() => {
+
+        });
+    })
+
+    return (
+        <SessionPage>
+
+        </SessionPage>
+    )
 }
