76782814c39ae0242b39311bc2a7fa1a727c427c
JorensM <jorensmerenjanu@gmail.com>
2024-01-17 09:27:48 +0200

    update secondary button to have gray border instead of green

--- app/src/styles/button.ts
+++ app/src/styles/button.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native'
 
 const button = StyleSheet.create({
     secondary: {
-        borderColor: 'green',
+        borderColor: 'gray',
         borderWidth: 1,
         width: '100%',
         padding: 8,
