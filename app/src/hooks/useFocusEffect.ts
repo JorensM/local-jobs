ca32de48071686a1552c5d21470d89471094e21e
JorensM <jorensmerenjanu@gmail.com>
2024-01-17 10:48:40 +0200

    add use_auth arg to useFocusEffect
    * this argument makes it so, if true, the effect only is run after the user has been fetched. Additionally it adds the auth.user state to deps
    * also added comments

--- app/src/hooks/useFocusEffect.ts
+++ app/src/hooks/useFocusEffect.ts
@@ -1,6 +1,29 @@
+// Core
 import { useFocusEffect as ExpoUseFocusEffect } from 'expo-router'
 import { useCallback } from 'react'
+import useAuth from './useAuth';
 
-export default function useFocusEffect(callback: () => void, deps?: any[]) {
-    return ExpoUseFocusEffect(useCallback(callback, deps || []))
+/**
+ * useFocusEffect wrapper
+ * @param callback Callback to call on focus
+ * @param deps Dependencies
+ * @param use_auth Whether to call this effect only after user has been fetched. If true, also adds auth.user to deps 
+ * @returns 
+ */
+export default function useFocusEffect(callback: () => void, deps?: any[], use_auth: boolean = false) {
+
+    const auth = useAuth();
+
+    const auth_dep = auth ? [auth.user] : []
+    const _deps = deps ? [...deps, ...auth_dep] : auth_dep
+    
+    return ExpoUseFocusEffect(
+        useCallback(() => {
+            if(use_auth && auth.user) {
+                callback()
+            } else if (!use_auth) {
+                callback()
+            }
+        }, deps || []) 
+    )
 }
