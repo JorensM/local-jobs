238140d9bcf28068fcd8d2cd5417136f6223ae48
JorensM <jorensmerenjanu@gmail.com>
2024-01-19 13:27:20 +0200

    refactor(ListSeparator): tidy up imports and add comments

--- app/src/components/layout/ListSeparator.tsx
+++ app/src/components/layout/ListSeparator.tsx
@@ -1,9 +1,17 @@
+// Core
 import { View } from 'react-native'
 
 type ListSeparatorProps = {
     height?: number
 }
 
+/**
+ * Basic list separator used for `FlatList`'s `itemSeparatorComponent` prop
+ * 
+ * ## Props
+ * 
+ * * `height` - height of the separator, default 8
+ */
 export default function ListSeparator( { height = 8 }: ListSeparatorProps) {
     return <View style={{height}} />
 }
