77e7da5893e0591a5972661950fd23928da6d55d
JorensM <jorensmerenjanu@gmail.com>
2024-01-19 14:16:17 +0200

    refactor(useContacts): tidy up imports, add ContactHook type and add comments

--- app/src/hooks/useContacts.ts
+++ app/src/hooks/useContacts.ts
@@ -1,49 +1,27 @@
+// Hooks
 import supabase from '#misc/supabase'
-import { User } from '#types/User';
 import useAuth from './useAuth'
 
-export default function useContacts() {
+// Types
+import { User } from '#types/User';
 
+type ContactsHook = {
+    fetchUsersContacts: () => Promise<User[]>
+    fetchContact: (contact_id: string) => Promise<User | null>
+}
+
+/**
+ * Hook used for managing contacts
+ * 
+ * ## Returns
+ * 
+ * `fetchUserContacts()` - fetches all of users contacts and returns an array of User objects of those contacts.
+ * `fetchContact()` - fetches single contact by id and returns User object of that contact
+ */
+export default function useContacts(): ContactsHook {
+
+    // Hooks
     const auth = useAuth();
-
-    const purchaseContact = async (contact_user_id: string) => {
-
-        if (!auth.user) {
-            throw new Error('User not stored in state')
-        }
-
-        {
-
-            const { data, error } = await supabase
-                .from('contacts')
-                .select()
-                .limit(1)
-                .eq('user_id', auth.user.id)
-                .eq('contact_id', contact_user_id)
-
-            if(error) {
-                throw error
-            }
-
-            if (data.length != 0) {
-                throw new Error('User already in your contacts')
-            }
-        }
-        
-
-        const { error } = await supabase
-            .from('contacts')
-            .insert({
-                user_id: auth.user.id,
-                contact_id: contact_user_id
-            })
-        
-        if(error) {
-            throw error
-        }
-
-        return true;
-    }
 
     const fetchUsersContacts = async () => {
 
@@ -82,35 +60,14 @@ export default function useContacts() {
             }))
 
             return users_parsed;
-
-            // const { data: _users, error } = await supabase
-            //     .from('users')
-            //     .select()
-            //     .in('id', contact_ids)
-
-            // if(error) {
-            //     throw error
-            // }
-
-            // users = _users;
         } else {
             return []
         }
 
-        
-
-        return users;
-    }
-
-    const parseUserData = (user_data: any): User => {
-        return {
-            id: user_data.user_id,
-            name: user_data.name,
-            role: user_data.role
-        }
     }
 
     const fetchContact = async (contact_id: string) => {
+
         const { data, error } = await supabase
             .from('user_data')
             .select()
@@ -120,14 +77,29 @@ export default function useContacts() {
         if(error) {
             throw error
         } else if (data.length == 0) {
-            throw new Error('Could not fetch contact')
+            return null;
         }
 
         return parseUserData(data[0]);
+
+    }
+
+    /**
+     * Convert DB user_data model to User object
+     * @param user_data user data as found in the user_data table in Supabase
+     * @returns {User} parsed User object
+     */
+    const parseUserData = (user_data: any): User => {
+
+        return {
+            id: user_data.user_id,
+            name: user_data.name,
+            role: user_data.role
+        }
+
     }
 
     return {
-        purchaseContact,
         fetchUsersContacts,
         fetchContact
     }
