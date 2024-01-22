3265674c908b1d23164f99704568014bc740b501
JorensM <jorensmerenjanu@gmail.com>
2024-01-22 11:06:46 +0200

    refactor(AuthContext): add comments

--- app/src/state/AuthContext.ts
+++ app/src/state/AuthContext.ts
@@ -2,10 +2,21 @@ import { User } from '#types/User';
 import { createContext } from 'react';
 
 type AuthContextType = {
+    /**
+     * Currently logged in user. Stores a User object or null if user not logged in
+     */
     user: User | null,
+    /**
+     * Set the currently logged in user state
+     * 
+     * @param new_user new User object to set user to, or null to unset user
+     */
     setUser: (new_user: User | null) => void
 }
 
+/**
+ * Auth related context
+ */
 const AuthContext = createContext<AuthContextType>({
     user: null,
     setUser: () => {}
