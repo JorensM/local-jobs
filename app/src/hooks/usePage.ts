6dc57e368a3d516e79ee6b935271818001a76244
JorensM <jorensmerenjanu@gmail.com>
2024-01-22 10:37:36 +0200

    refactor(usePage): create type for hook and add comments to support JSDoc

--- app/src/hooks/usePage.ts
+++ app/src/hooks/usePage.ts
@@ -2,22 +2,43 @@
 import { usePathname } from 'expo-router/src/hooks';
 import { useEffect, useMemo, useState } from 'react';
 
+// Types
+import { PageState } from '#types/PageState';
+
+type PageHook = {
+    /**
+     * Use this to set the error message to display on the page. Set to null
+     * to hide error message. If error message is present, it will be displayed
+     * even if page is in loading state
+     * 
+     * @param message message to display or null to hide message
+     */
+    setError: (message: string | null) => void
+    /**
+     * Use this to set the loading state of the page as a boolean
+     * 
+     * @param is_loading whether page should be in loading state or not
+     */
+    setLoading: (is_loading: boolean) => void
+    /**
+     * Pass this to the Page/SessionPage component's `pageState` prop so it can display the state
+     */
+    pageState: PageState
+}
+
 /**
  * Hook to handle page state.
- *  
- * @returns object with following properties:
- * 
- * * **setError** - Use this to set the error message to display on the page. Set to null
- * to hide error message
- * * **setLoading** - Use this to set the loading state of the page as a boolean
- * * **pageState** - Pass this to the Page/SessionPage component's `pageState` prop so it can display the state
  */
-export default function usePage(initialLoading: boolean = false) {
+export default function usePage(initialLoading: boolean = false): PageHook {
     const pathname = usePathname()
 
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState<boolean>(initialLoading);
 
+    /* 
+        Reset loading state to initial loading state upon route change to prevent
+        flickering when switching pages.
+    */
     useEffect(() => {
         setLoading(initialLoading)
     }, [pathname])
@@ -26,6 +47,7 @@ export default function usePage(initialLoading: boolean = false) {
         error,
         loading
     }), [error, loading])
+
     return {
         setError,
         setLoading,
