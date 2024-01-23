e9bb81590374e2f3a44f0bee82482c90b5a2f15f
JorensM <jorensmerenjanu@gmail.com>
2024-01-23 12:51:01 +0200

    refactor(tests): fix useAuth mock not working

--- app/src/tests/mocks/hooks/useAuth.ts
+++ app/src/tests/mocks/hooks/useAuth.ts
@@ -10,21 +10,26 @@ jest.mock('#hooks/useAuth', () => {
     // const { useContext } = jest.requireActual('react');
     // const AuthContext = jest.requireActual('#state/AuthContext');
 
-    const { user, setUser } = mockUseContext(mockAuthContext);
+    // const { user, setUser } = mockUseContext(mockAuthContext);
     let current_user: User | null = null;
 
     return {
         __esModule: true,
         default: () => ({
             login: async (email: string, password: String) => {
-                setUser({
+                // setUser({
+                //     id: email,
+                //     name: email,
+                //     role: 'performer'
+                // })
+                current_user = {
                     id: email,
                     name: email,
                     role: 'performer'
-                })
+                }
                 return true;
             },
-            user
+            user: current_user
         })
     }
 })
