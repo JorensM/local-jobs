493256fd92c3777ab8ebcd919a36a4da07373fd1
JorensM <jorensmerenjanu@gmail.com>
2024-01-23 08:48:52 +0200

    revert supabase to before test client

--- app/src/misc/supabase.ts
+++ app/src/misc/supabase.ts
@@ -7,12 +7,6 @@ import { createBrowserClient } from '@supabase/ssr/dist/index'; //Need to explic
 // Constants
 import { SUPABASE_KEY, SUPABASE_URL } from '#constants/env';
 
-// Testing
-import * as AsyncStorageTest from '@react-native-async-storage/async-storage/jest/async-storage-mock'
-
-const test_storage: any = {}
-
-console.log(process.env.NODE_ENV)
 
 /**
  * Supabase client
@@ -29,17 +23,18 @@ const supabase = createBrowserClient(
         },
         cookies: { // This is not being used anywhere but is required by TS for some reason
             get: async (key: string) => {
-                console.log('getting cookie')
-                return test_storage[key]
+                //console.log('getting cookie')
+                return null;//test_storage[key]
             },
             set: async(key: string, value: string) => {
-                test_storage[key] = value
+                //test_storage[key] = value
             },
             remove: async (key: string) => {
-                delete test_storage[key]
+                //delete test_storage[key]
             }
         }
     }
 )
+
 
 export default supabase
