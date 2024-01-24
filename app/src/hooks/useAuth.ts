a7174ce9cc745e5110747817aa666c18a00cf3b1
JorensM <jorensmerenjanu@gmail.com>
2024-01-24 09:24:02 +0200

    useAuth: remove unneeded console.logs

--- app/src/hooks/useAuth.ts
+++ app/src/hooks/useAuth.ts
@@ -54,19 +54,15 @@ export default function useAuth(): AuthHook {
     const context = useContext(AuthContext)
 
     const login = async (email: string, password: string): Promise<true> => {
-        console.log('logging in aaaa')
         const { error } = await supabase.auth.signInWithPassword({
             email,
             password
         })
-        console.log('2')
         if (error){
             throw error
         }
-        console.log('3')
         fetchUser();
-
-        console.log('4')
+        
         return true;
     }
 
