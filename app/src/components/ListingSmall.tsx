66e5731db0f9f1972cab609daef66b582c7507b4
JorensM <jorensmerenjanu@gmail.com>
2024-01-25 14:09:16 +0200

    refactor(components): add comments to ListingSmall, extract title style to ./styles, remove unused styles and comment out unused code

--- app/src/components/ListingSmall.tsx
+++ app/src/components/ListingSmall.tsx
@@ -9,39 +9,43 @@ import Description from './typography/Description'
 
 // Types
 import { Listing } from '#types/Listing'
+
+// Misc
 import getAge from '#misc/getAge'
+
+// Styles
+import list from '#styles/list'
+import text from '#styles/text'
 
 //Functions
 //import getAge from '#functions/getAge'
 //import usePlacesAPI from '../functions/usePlacesAPI'
 
 type Props = {
+    /**
+     * Listing object
+     */
     item: Listing
+    /**
+     * called when card is pressed
+     */
     onPress: () => void
 }
 
 /**
  * Listing card, used to display basic info about a listing.
- * 
- * ## Props
- * 
- * * `item` - Listing object
- * * `onPress` - called when card is pressed
  */
 export default function ListingSmall( { item, onPress }: Props ) {
 
-    //const { getPlaceByID } = usePlacesAPI()
+    
+
+    // State
 
     const [ locationStr, setLocationStr ] = useState<string>('')
 
-    const handleListingPress = useCallback(() => {
-        // console.log('listing button press: ' + item.$id)
-        // navigation.navigate('Listing', {
-        //     id: item.$id
-        // })
-    }, [])
-
-    const fetchLocationStr = () => {
+    // Keep this for now as it may be needed when implementing location field
+    //const { getPlaceByID } = usePlacesAPI()
+    //const fetchLocationStr = () => {
         // if (!item.location_id) {
         //     return
         // }
@@ -52,25 +56,26 @@ export default function ListingSmall( { item, onPress }: Props ) {
         //     .catch(err => {
         //         console.error('Could not fetch location by ID' + item.location_id, err)
         //     })
-    }
-
-    useEffect(() => {
+    //}
+    //useEffect(() => {
         // if (item.location_id) {
         //     fetchLocationStr()
         // }
-    }, [])
+    //}, [])
 
     return (
         <Pressable
-            style={ styles.listing_item }
+            style={ list.item }
             onPress={ onPress }
         >
+            {/* Listing title */}
             <Text
-                style={styles.title}
+                style={text.title}
             >
                 
                 { item.title }
             </Text>
+            {/* Listing author */}
             <Caption>
                 By { item.user_name }
             </Caption>
@@ -79,10 +84,11 @@ export default function ListingSmall( { item, onPress }: Props ) {
                     { item.location_name }
                 </Caption>
             : null } */}
-            
+            {/* Listing age */}
             <Caption>
                 { getAge(item.created_at, true) } ago
             </Caption>
+            {/* Listing description, only showing first 100 characters */}
             <Description>
                 { item.description.slice(0, 100) }...
             </Description>
@@ -91,17 +97,3 @@ export default function ListingSmall( { item, onPress }: Props ) {
         </Pressable>
     )
 }
-
-const styles = StyleSheet.create({
-    title: {
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-    listing_item: {
-        padding: 4,
-        width: '100%',
-        borderWidth: 1,
-        borderRadius: 4,
-        borderColor: 'gray'
-    }
-})
--- app/src/styles/text.ts
+++ app/src/styles/text.ts
@@ -4,7 +4,11 @@ import { StyleSheet } from 'react-native';
 const text = StyleSheet.create({
     warn: {
         color: 'red'
-    }
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
 })
 
 export default text;
