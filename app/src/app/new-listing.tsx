c0a7f2439cc2395c50dfbf2b304575e59428dd13
JorensM <jorensmerenjanu@gmail.com>
2024-01-25 12:53:14 +0200

    refactor(pages): add comments to new-listing page

--- app/src/app/new-listing.tsx
+++ app/src/app/new-listing.tsx
@@ -24,21 +24,32 @@ export default function NewListingPage() {
     const { pageState } = usePage();
     
     // Handlers
+
+    /**
+     * On submit button press. Creates new listing and redirecs to the newly created
+     * listing's page
+     * @param values Formik form values
+     */
     const handleSubmit = async (values: ListingFormValues) => {
 
         try {
+            // Create listing with provided form values and user's data
             const id = await listings.createListing({
                 ...values,
                 user_id: auth.user!.id,
                 user_name: auth.user!.name
             })
+            // If listing was created successfully, redirect to the new listing's page
+            // and display a success toast
             if(id) {
                 router.replace('listings/' + id);
                 toastSuccess('Success', 'Listing has been created');
             } else {
+                // Otherwise throw error
                 throw new Error('Could not create listing');
             }
         } catch(error: any) {
+            // Display error if there was one
             toastError('Error', error.message);
         }
         
@@ -48,6 +59,7 @@ export default function NewListingPage() {
         <SessionPage
             pageState={pageState}
         >
+            {/* New listing form */}
             <ListingForm 
                 onSubmit={handleSubmit}
             />
