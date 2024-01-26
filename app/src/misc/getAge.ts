110b44d367422785b50e41e171617e209ac9148f
JorensM <jorensmerenjanu@gmail.com>
2024-01-26 10:23:57 +0200

    refactor(misc): add comments

--- app/src/misc/getAge.ts
+++ app/src/misc/getAge.ts
@@ -2,9 +2,10 @@ import humanize from 'humanize-duration';
 
 /**
  * Convert milliseconds to human readable time format
- * @param ms 
+ * @param ms time in milliseconds
  */
 const humanizeTime = (ms: number) => {
+    // Use 'humanize-duration' library to humanize time and return the formatted time
     return humanize(
         ms,
         {
@@ -17,14 +18,15 @@ const humanizeTime = (ms: number) => {
 
 /**
  * Get time span from given time to current time in ms
+ * 
  * @param { number | string } time Time as a datetime string or a number in ms 
  * @param { boolean } humanize Whether to humanize the output as a human readable string
- * @returns { number | string } Number in ms if humanize is `false`, string if humanize is `true`
+ * 
+ * @returns { number | string } Number in ms if humanize is `false`, formatted time string if humanize is `true`
  */
 export default function getAge (time: number | string, humanize = false): number | string {
-    //console.log( time )
-    //console.log( new Date() )
-    //console.log( new Date(time) )
+    // Calculate age
     const age = new Date().getTime() - new Date(time).getTime()
+    // Return calculated age, optionally humanizing the time to make it readable
     return humanize ? humanizeTime(age) : age
 }
