d8c1001e66a78530ba3eee1fac11e8573b68caea
JorensM <jorensmerenjanu@gmail.com>
2024-01-26 10:27:09 +0200

    refactor(misc): add comments

--- app/src/misc/route_utils.ts
+++ app/src/misc/route_utils.ts
@@ -7,6 +7,7 @@ import { user_routes, guest_routes } from '#constants/routes'
  * @returns sanitized route name
  */
 const sanitizeRouteName = (name: string): string => {
+    // If route name starts with a slash, remove it
     if( name.startsWith('/') ) {
         return name.substring(1)
     }
